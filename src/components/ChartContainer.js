6ec80e505e5592e2e5caaacae91ae11772c8240e
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-06-12 01:55:11 +0300

    dragging and resizing with bug

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,14 +1,11 @@
 import Draggable from './Draggable'
-import Resizable from './Resizable'
 import './ChartContainer.css'
 
 
 const ChartContainer = () => {
   return (
     <Draggable>
-      <Resizable>
         <div className='ChartContainer'></div>
-      </Resizable>
     </Draggable>
   )
 }
--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react"
 import styled from 'styled-components'
+import './Draggable.css'
 
-const Container = styled.div.attrs(({ x, y }) => ({
+const Container = styled.div.attrs(({ x, y, width, height }) => ({
   style: {
-    transform: `translate(${x}px, ${y}px)`
+    transform: `translate(${x}px, ${y}px)`,
+    width: width,
+    height: height
   }
 }))`
   cursor: grab;
@@ -14,10 +17,13 @@ const Container = styled.div.attrs(({ x, y }) => ({
     `
   }
   display: inline-block;
+  position: relative;
 `
 
 const Draggable = (props) => {
+  const [size, setSize] = useState({ width: 100, height: 100 })
   const [dragging, setDragging] = useState(false)
+  const [resizing, setResizing] = useState(false)
   const [position, setPosition] = useState({
     x: 0,
     y: 0
@@ -33,9 +39,12 @@ const Draggable = (props) => {
     y: 0
   })
 
-  const handleMouseUp = ({clientX, clientY}) => {
-    window.removeEventListener('mousemove', handleMouseMove)
-    window.removeEventListener('mouseup', handleMouseUp)
+  const [resizingClick, setResizingClick] = useState(null)
+
+  const handleMouseUpDragging = ({ clientX, clientY }) => {
+    console.log('handleMouseUpDragging')
+    window.removeEventListener('mousemove', handleMouseMoveDragging)
+    window.removeEventListener('mouseup', handleMouseUpDragging)
     setPosition({
       x: clientX - diff.x,
       y: clientY - diff.y,
@@ -45,25 +54,87 @@ const Draggable = (props) => {
 
   useEffect(() => {
     if (dragging) {
-      window.addEventListener('mousemove', handleMouseMove)
-      window.addEventListener('mouseup', handleMouseUp)
+      console.log('useEffect dragging')
+      window.addEventListener('mousemove', handleMouseMoveDragging)
+      window.addEventListener('mouseup', handleMouseUpDragging)
     }
   }, [dragging])
 
-  const handleMouseMove = ({ clientX, clientY }) => {
+  useEffect(() => {
+    if (resizing) {
+      console.log('useEffect resizing')
+      window.addEventListener('mousemove', handleMouseMoveResizing)
+      window.addEventListener('mouseup', handleMouseUpResizing)
+    }
+  }, [resizing])
+
+  const handleMouseMoveDragging = ({ clientX, clientY }) => {
+    console.log('handleMouseMoveDragging')
     setTranslate({
       x: clientX - diff.x,
       y: clientY - diff.y
     })
   }
 
-  const handleMouseDown = ({ clientX, clientY }) => {
+  const handleMouseDownDragging = ({ clientX, clientY }) => {
+    console.log('handleMouseDownDragging')
     setDiff({ x: clientX - position.x, y: clientY - position.y })
     setDragging(true)
   }
 
+  const handleMouseDownResizing = (e) => {
+    console.log('handleMouseDownResizing')
+    e.stopPropagation()
+    setResizing(true)
+    setResizingClick({ x: e.clientX, y: e.clientY, directions: Array.from(e.target.classList) })
+  }
+
+  const handleMouseMoveResizing = ({clientX, clientY}) => {
+    console.log('handleMouseDownResizing')
+    const diffX = clientX - resizingClick.x
+    const diffY = clientY - resizingClick.y
+    let {width, height} = size
+    if (resizingClick.directions.includes('left') || resizingClick.directions.includes('right')) {
+      width += diffX
+    }
+    if (resizingClick.directions.includes('top') || resizingClick.directions.includes('bottom')) {
+      height += diffY
+    }
+    setSize({width, height})
+    setResizingClick({ ...resizingClick, x: clientX, y: clientY })
+  }
+
+  const handleMouseUpResizing = () => {
+    console.log('handleMouseUpResizing')
+    window.removeEventListener('mousemove', handleMouseMoveResizing)
+    window.removeEventListener('mouseup', handleMouseUpResizing)
+    setResizingClick(null)
+    setResizing(false)
+  }
+
   return (
-    <Container onMouseDown={handleMouseDown} x={translate.x} y={translate.y} dragging={dragging} className="draggable">
+    <Container
+      width={size.width}
+      height={size.height}
+      onMouseDown={handleMouseDownDragging}
+      x={translate.x}
+      y={translate.y}
+      dragging={dragging}
+      className="draggable">
+      <div className='resizers' onMouseDown={handleMouseDownResizing}>
+        <div className='lines'>
+          <div className='resizer left'></div>
+          <div className='resizer top'></div>
+          <div className='resizer right'></div>
+          <div className='resizer bottom'></div>
+        </div>
+        <div className='angles'>
+          <div className='resizer top left'></div>
+          <div className='resizer top right'></div>
+          <div className='resizer bottom left'></div>
+          <div className='resizer bottom right'></div>
+        </div>
+      </div>
       {props.children}
     </Container>
   )
