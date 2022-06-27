a57731b9a416acf6ddb83583eda859322d145290
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-06-27 19:21:47 +0300

    Squashed commit of the following:
    commit c985ab993f47cd4a6ce653b1ccf5c5ddbf020d25
    Author: Nikolay Tyukaev <ntyukaev@gmail.com>
    Date:   Mon Jun 27 19:20:10 2022 +0300
    
        top menu initial layout

--- src/components/layout/TopMenu/TopMenu.js
+++ src/components/layout/TopMenu/TopMenu.js
@@ -1,15 +1,23 @@
-import { Layout } from 'antd'
+import { Layout, Row, Col } from 'antd'
+import './TopMenu.css'
 
 const { Header } = Layout
 
 const TopMenu = () => {
   return (
     <Header
-      className="site-layout-background"
+      className='top-menu'
       style={{
         padding: 0,
       }}
-    />
+    >
+      <Row>
+        <Col span={6}>Logo</Col>
+        <Col span={6}>Import Data</Col>
+        <Col span={6}>Other Menu</Col>
+        <Col span={6}>Login</Col>
+      </Row>
+    </Header>
   )
 }
 
